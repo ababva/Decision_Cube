59033b3d5f40073b0f588b0329f40e3f199eea3e
1RAFTIK1 <talapov.dima1@gmail.com>
2025-11-16 02:12:12 +0300

    создан файл LoginScreen.kt - вынесен LoginScreen

---
+++ app/src/main/java/com/decisioncube/app/screens/LoginScreen.kt
@@ -0,0 +1,18 @@
+
+
+package com.decisioncube.app.screens
+
+
+import androidx.compose.material3.Text
+
+
+import androidx.compose.runtime.Composable
+
+
+@Composable fun LoginScreen(onLogin: () -> Unit) {
+Text("Login Screen Placeholder")
+
+
+}
+
+
